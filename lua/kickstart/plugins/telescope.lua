1b7575be1ec5233b8db0ba238f16a939df7d906a
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-05-14 09:30:29 +0200

    small change to keep inline with base

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -87,13 +87,13 @@ return {
 
       vim.keymap.set('n', '<leader>/', builtin.live_grep, { desc = 'Grep (cwd)' })
       -- Slightly advanced example of overriding default behavior and theme
-      -- vim.keymap.set('n', '<leader>/', function()
-      --   -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      --   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-      --     winblend = 10,
-      --     previewer = false,
-      --   })
-      -- end, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader>sb', function()
+        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[S]earch [B]uffer (fuzzily)' })
 
       -- It's also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys

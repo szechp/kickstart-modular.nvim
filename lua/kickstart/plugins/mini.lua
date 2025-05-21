6b0407e7c83e78ddcaf10775b2a786cee662b59b
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-05-21 22:21:47 +0200

    restore here: if you need the old mini config

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -20,61 +20,52 @@ return
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
-      require('mini.jump2d').setup()
-      -- https://github.com/echasnovski/mini.nvim/wiki/mini.jump2d#replace-f-f-t-t
-      local function make_fftt_keymap(key, extra_opts)
+      require('mini.jump2d').setup { mappings = { start_jumping = '' } }
+      local function make_fFtT_keymap(key, extra_opts)
         local opts = vim.tbl_deep_extend('force', { allowed_lines = { blank = false, fold = false } }, extra_opts)
         opts.hooks = opts.hooks or {}
 
         opts.hooks.before_start = function()
           local input = vim.fn.getcharstr()
-        --stylua: ignore
-        if input == nil then
-          opts.spotter = function() return {} end
-        else
-          local pattern = vim.pesc(input)
-          opts.spotter = minijump2d.gen_pattern_spotter(pattern)
-        end
+          --stylua: ignore
+          if input == nil then
+            opts.spotter = function() return {} end
+          else
+            local pattern = vim.pesc(input)
+            opts.spotter = MiniJump2d.gen_pattern_spotter(pattern)
+          end
         end
 
-        -- using `<cmd>...<cr>` enables dot-repeat in operator-pending mode
-        _g.jump2dfftt_opts = _g.jump2dfftt_opts or {}
-        _g.jump2dfftt_opts[key] = opts
-        local command = string.format('<cmd>lua minijump2d.start(_g.jump2dfftt_opts.%s)<cr>', key)
+        -- Using `<Cmd>...<CR>` enables dot-repeat in Operator-pending mode
+        _G.jump2dfFtT_opts = _G.jump2dfFtT_opts or {}
+        _G.jump2dfFtT_opts[key] = opts
+        local command = string.format('<Cmd>lua MiniJump2d.start(_G.jump2dfFtT_opts.%s)<CR>', key)
 
         vim.api.nvim_set_keymap('n', key, command, {})
         vim.api.nvim_set_keymap('v', key, command, {})
         vim.api.nvim_set_keymap('o', key, command, {})
       end
 
-      make_fftt_keymap('f', { allowed_lines = { cursor_before = false } })
-      make_fftt_keymap('f', { allowed_lines = { cursor_after = false } })
-      make_fftt_keymap('t', {
-        allowed_lines = { cursor_before = false },
-        hooks = {
-          after_jump = function()
-            vim.api.nvim_input '<left>'
-          end,
-        },
-      })
-      make_fftt_keymap('t', {
-        allowed_lines = { cursor_after = false },
-        hooks = {
-          after_jump = function()
-            vim.api.nvim_input '<right>'
-          end,
-        },
-      })
       require('mini.icons').setup()
+      make_fFtT_keymap('f', { allowed_lines = { cursor_before = false } })
+      make_fFtT_keymap('F', { allowed_lines = { cursor_after = false } })
+      make_fFtT_keymap('t', {
+        allowed_lines = { cursor_before = false },
+        hooks = { after_jump = function() vim.api.nvim_input('<Left>') end },
+      })
+      make_fFtT_keymap('T', {
+        allowed_lines = { cursor_after = false },
+        hooks = { after_jump = function() vim.api.nvim_input('<Right>') end },
+      })
       require('mini.pairs').setup()
-      require('mini.indentscope').setup {
+      require('mini.indentscope').setup({
         draw = {
           animation = require('mini.indentscope').gen_animation.none(),
         },
-      }
-      -- add/delete/replace surroundings (brackets, quotes, etc.)
+      })
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
-      -- - saiw) - [s]urround [a]dd [i]nner [w]ord [)]paren
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup {

16a33aca8861a9b1c82e4fb397d6eb6385233aff
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-06-12 13:42:33 +0200

    small keybind changes

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -8,6 +8,20 @@ return {
       {
         'MeanderingProgrammer/render-markdown.nvim',
         ft = { 'markdown', 'codecompanion' },
+        keys = {
+          -- Accept next word with Alt+Right
+          { '<M-Right>', '<Plug>(copilot-accept-word)', mode = 'i', desc = 'Copilot Accept Word' },
+          -- Accept next line with Alt+Ctrl+Right
+          { '<M-C-Right>', '<Plug>(copilot-accept-line)', mode = 'i', desc = 'Copilot Accept Line' },
+          -- Cycle to next suggestion with Alt+]
+          { '<M-]>', '<Plug>(copilot-next)', mode = 'i', desc = 'Copilot Next Suggestion' },
+          -- Cycle to previous suggestion with Alt+[
+          { '<M-[>', '<Plug>(copilot-previous)', mode = 'i', desc = 'Copilot Previous Suggestion' },
+          -- Dismiss suggestion
+          { '<C-]>', '<Plug>(copilot-dismiss)', mode = 'i', desc = 'Copilot Dismiss Suggestion' },
+          -- Request suggestion explicitly
+          { '<M-\\>', '<Plug>(copilot-suggest)', mode = 'i', desc = 'Copilot Request Suggestion' },
+        },
       },
     },
     config = function()

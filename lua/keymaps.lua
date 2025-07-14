07b12437c6c1d0e9fe87a293010ab9655f7bcbae
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-07-14 11:11:57 +0200

    remove custom yaml thing

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -50,30 +50,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ my custom stuff ]]
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'yaml',
-  callback = function(args)
-    local bufnr = args.buf
-    -- Wait for the yaml-language-server to start
-    local clients = vim.lsp.get_clients({ name = 'yamlls', bufnr = bufnr })
-    if #clients > 0 then
-      -- If the server is already running, call init()
-      require('custom.yaml-k8s-crds').init(bufnr)
-    else
-      -- If the server is not running, wait for it to start
-      vim.api.nvim_create_autocmd('LspAttach', {
-        once = true,
-        buffer = bufnr,
-        callback = function(lsp_args)
-          local client = vim.lsp.get_client_by_id(lsp_args.data.client_id)
-          if client and client.name == 'yamlls' then
-            require('custom.yaml-k8s-crds').init(bufnr)
-          end
-        end,
-      })
-    end
-  end,
-})
 
 vim.api.nvim_set_keymap("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc = "Select all" })
 

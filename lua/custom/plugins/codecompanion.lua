8cc26fba4f90c025558c4822563eba544efcbc92
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-08-06 12:51:43 +0200

    use local llms with codecompanion

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -24,7 +24,8 @@ return {
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-treesitter/nvim-treesitter',
-      'github/copilot.vim',
+      -- 'github/copilot.vim',
+      'ravitemer/codecompanion-history.nvim',
       {
         'Davidyz/VectorCode',
         version = '*', -- optional, depending on whether you're on nightly or release
@@ -34,20 +35,20 @@ return {
       {
         'MeanderingProgrammer/render-markdown.nvim',
         ft = { 'markdown', 'codecompanion' },
-        keys = {
-          -- Accept next word with Alt+Right
-          { '<M-Right>', '<Plug>(copilot-accept-word)', mode = 'i', desc = 'Copilot Accept Word' },
-          -- Accept next line with Alt+Ctrl+Right
-          { '<M-C-Right>', '<Plug>(copilot-accept-line)', mode = 'i', desc = 'Copilot Accept Line' },
-          -- Cycle to next suggestion with Alt+]
-          { '<M-]>', '<Plug>(copilot-next)', mode = 'i', desc = 'Copilot Next Suggestion' },
-          -- Cycle to previous suggestion with Alt+[
-          { '<M-[>', '<Plug>(copilot-previous)', mode = 'i', desc = 'Copilot Previous Suggestion' },
-          -- Dismiss suggestion
-          { '<C-]>', '<Plug>(copilot-dismiss)', mode = 'i', desc = 'Copilot Dismiss Suggestion' },
-          -- Request suggestion explicitly
-          { '<M-\\>', '<Plug>(copilot-suggest)', mode = 'i', desc = 'Copilot Request Suggestion' },
-        },
+        -- keys = {
+        --   -- Accept next word with Alt+Right
+        --   { '<M-Right>', '<Plug>(copilot-accept-word)', mode = 'i', desc = 'Copilot Accept Word' },
+        --   -- Accept next line with Alt+Ctrl+Right
+        --   { '<M-C-Right>', '<Plug>(copilot-accept-line)', mode = 'i', desc = 'Copilot Accept Line' },
+        --   -- Cycle to next suggestion with Alt+]
+        --   { '<M-]>', '<Plug>(copilot-next)', mode = 'i', desc = 'Copilot Next Suggestion' },
+        --   -- Cycle to previous suggestion with Alt+[
+        --   { '<M-[>', '<Plug>(copilot-previous)', mode = 'i', desc = 'Copilot Previous Suggestion' },
+        --   -- Dismiss suggestion
+        --   { '<C-]>', '<Plug>(copilot-dismiss)', mode = 'i', desc = 'Copilot Dismiss Suggestion' },
+        --   -- Request suggestion explicitly
+        --   { '<M-\\>', '<Plug>(copilot-suggest)', mode = 'i', desc = 'Copilot Request Suggestion' },
+        -- },
       },
     },
     config = function()
@@ -55,7 +56,25 @@ return {
         log_level = 'DEBUG',
         strategies = {
           chat = {
-            adapter = 'copilot',
+            adapter = 'ollama',
+            opts = {
+              model = 'qwen2.5-coder:14b', -- or whatever model you want, e.g. 'codellama:7b-instruct'
+              url = 'http://localhost:11434', -- optional, default is this
+            },
+          },
+          inline = {
+            adapter = 'ollama',
+            opts = {
+              model = 'qwen2.5-coder:14b', -- or whatever model you want, e.g. 'codellama:7b-instruct'
+              url = 'http://localhost:11434', -- optional, default is this
+            },
+          },
+          cmd = {
+            adapter = 'ollama',
+            opts = {
+              model = 'qwen2.5-coder:14b', -- or whatever model you want, e.g. 'codellama:7b-instruct'
+              url = 'http://localhost:11434', -- optional, default is this
+            },
           },
         },
         display = {

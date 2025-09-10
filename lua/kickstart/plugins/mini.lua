f29eb98aae3d2ec01502154f9a57141a43db0918
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-09-10 12:12:44 +0200

    formatting

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -82,6 +82,7 @@ return {
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
+
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN

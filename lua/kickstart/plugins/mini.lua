0241fcaa4427afbd25cbce036f8293e17f90d470
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-09-10 12:09:56 +0200

    get inline with upstream 3

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -51,9 +51,6 @@ return {
       require('mini.diff').setup {
         source = require('mini.diff').gen_source.none(),
       }
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
       require('mini.tabline').setup()
 
       require('mini.pairs').setup()
@@ -78,6 +75,18 @@ return {
           update_n_lines = 'gsn',
         },
       }
+
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function() return '%2l:%-2v' end
 
       require('mini.bufremove').setup()
       -- require('mini.clue').setup({

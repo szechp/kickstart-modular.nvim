dc9a28187febacf9473d0b01f85dfd388c2efdaf
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-09-10 12:00:12 +0200

    fix typo

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -25,7 +25,7 @@ vim.keymap.set('n', '<leader>Q', ':bd<CR>', { desc = 'Close current buffer' })
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easckier.
+-- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands

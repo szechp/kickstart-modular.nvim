233c756bebc1c5580dac8a09ddbc28948435809d
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2026-01-30 10:53:09 +0100

    fix noice breaking with newer treesitter

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,42 +1,51 @@
 return {
-  "folke/noice.nvim",
+  'folke/noice.nvim',
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
+    'MunifTanjim/nui.nvim',
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     -- "rcarriga/nvim-notify",
-    },
+  },
   enabled = not vim.g.vscode,
-  event = "VeryLazy",
+  event = 'VeryLazy',
   opts = {
-      views = {
-        mini = {
-          position = {
-            row = 1,
-            col = -2, -- negative col means offset from the right
-          },
+    cmdline = {
+      format = {
+        cmdline = { lang = '' },
+        search_down = { lang = '' },
+        search_up = { lang = '' },
+        filter = { lang = '' },
+        lua = { lang = '' },
+      },
+    },
+    views = {
+      mini = {
+        position = {
+          row = 1,
+          col = -2, -- negative col means offset from the right
         },
       },
+    },
     lsp = {
       override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
       },
     },
     routes = {
       {
         filter = {
-          event = "msg_show",
+          event = 'msg_show',
           any = {
-            { find = "%d+L, %d+B" },
-            { find = "; after #%d+" },
-            { find = "; before #%d+" },
+            { find = '%d+L, %d+B' },
+            { find = '; after #%d+' },
+            { find = '; before #%d+' },
           },
         },
-        view = "mini",
+        view = 'mini',
       },
     },
     presets = {
@@ -61,9 +70,9 @@ return {
     -- HACK: noice shows messages from before it was enabled,
     -- but this is not ideal when Lazy is installing plugins,
     -- so clear the messages in this case.
-    if vim.o.filetype == "lazy" then
-      vim.cmd([[messages clear]])
+    if vim.o.filetype == 'lazy' then
+      vim.cmd [[messages clear]]
     end
-    require("noice").setup(opts)
+    require('noice').setup(opts)
   end,
 }
